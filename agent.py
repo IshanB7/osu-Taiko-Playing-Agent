bbe775e17e29276377f063eee00db761481d2af3
Ishan <ishanb1569@gmail.com>
2025-04-07 11:57:43 -0400

    added exit_flag to avoid using interrupts

--- agent.py
+++ agent.py
@@ -18,34 +18,47 @@ interval = 1 / 30.
 next_frame_time = time.time()
 
 capture = False
+exit_flag = False
+
 def on_press(key):
     pass
 
 def on_release(key):
     global capture
+    global exit_flag
+    
     if key == keyboard.Key.enter:
         capture = not capture
+
+    if key == keyboard.Key.esc:
+        exit_flag = True
+        return False
 
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 listener.start()
 
-while not capture:
-    time.sleep(0.1)
+while True:
 
-while capture:
-    sleep_time = next_frame_time - time.time()
-    if sleep_time > 0:
-        time.sleep(sleep_time)
-    next_frame_time += interval
+    while not capture and not exit_flag:
+        time.sleep(0.1)
 
-    with mss.mss() as sct:
-        img = sct.grab(region)
-        img = np.array(img)
+    if exit_flag:
+        break
 
-    image = cv2.resize(img[:, :, :3], (16, 16))
+    while capture and not exit_flag:
+        sleep_time = next_frame_time - time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        next_frame_time += interval
 
-    img_array = image / 255.0
-    input_img = np.expand_dims(img_array, axis=0)
-    press = np.argmax(model(input_img)[0])
-    pyautogui.press(ACTIONS[press], _pause=False)
-    # pyautogui.press(ACTIONS[press])
+        with mss.mss() as sct:
+            img = sct.grab(region)
+            img = np.array(img)
+
+        image = cv2.resize(img[:, :, :3], (16, 16))
+
+        img_array = image / 255.0
+        input_img = np.expand_dims(img_array, axis=0)
+        press = np.argmax(model(input_img)[0])
+        pyautogui.press(ACTIONS[press], _pause=False)
+        # pyautogui.press(ACTIONS[press])
